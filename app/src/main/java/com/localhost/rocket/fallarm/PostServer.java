01bd8a2781464455e3594c874bc5cc1ce38528a0
r0ck37 <naveen91@gmail.com>
2016-07-30 12:43:06 -0700

    fallarm commit

---
+++ app/src/main/java/com/localhost/rocket/fallarm/PostServer.java
@@ -0,0 +1,34 @@
+package com.localhost.rocket.fallarm;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by rocket on 7/13/16.
+ */
+
+public class PostServer {
+    public final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+
+    String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    String bowlingJson(String s) {
+        return "{" + s + "}";
+    }
+}
